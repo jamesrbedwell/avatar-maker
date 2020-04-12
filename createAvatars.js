c9fb65969ee1583ce65f80be3cbc5eefee5a6ad1
James Bedwell <james@theredfox.group>
2020-04-12 15:06:07 +1000

    add some try catches

--- createAvatars.js
+++ createAvatars.js
@@ -17,40 +17,44 @@ const checkVal = (val, newMeasurement, compareVal) => {
 };
 
 const createAvatar = async (filename) => {
-	await faceapi.nets.ssdMobilenetv1.loadFromDisk('./weights');
-	await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights');
-	const img = await canvas.loadImage(`./images/${filename}`);
-	const landmarks = await faceapi.detectFaceLandmarks(img);
-	const dimensions = await faceapi.getMediaDimensions(img);
-	const smallestEdge = Math.floor(
-		Math.min(dimensions._width, dimensions._height)
-	);
-	const nose = landmarks.getNose();
+	try {
+		await faceapi.nets.ssdMobilenetv1.loadFromDisk('./weights');
+		await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights');
+		const img = await canvas.loadImage(`./images/${filename}`);
+		const landmarks = await faceapi.detectFaceLandmarks(img);
+		const dimensions = await faceapi.getMediaDimensions(img);
+		const smallestEdge = Math.floor(
+			Math.min(dimensions._width, dimensions._height)
+		);
+		const nose = landmarks.getNose();
 
-	const topNoseX = nose[1]._x;
-	const topNoseY = nose[1]._y;
+		const topNoseX = nose[1]._x;
+		const topNoseY = nose[1]._y;
 
-	const halfDimension = Math.floor(smallestEdge / 2);
-	const left = Math.floor(topNoseX - halfDimension);
-	const top = Math.floor(topNoseY - halfDimension);
+		const halfDimension = Math.floor(smallestEdge / 2);
+		const left = Math.floor(topNoseX - halfDimension);
+		const top = Math.floor(topNoseY - halfDimension);
 
-	const newLeft = left > 0 ? left : 0;
-	const newTop = top > 0 ? top : 0;
+		const newLeft = left > 0 ? left : 0;
+		const newTop = top > 0 ? top : 0;
 
-	const checkedLeft = checkVal(newLeft, smallestEdge, dimensions._width);
-	const checkedTop = checkVal(newTop, smallestEdge, dimensions._height);
+		const checkedLeft = checkVal(newLeft, smallestEdge, dimensions._width);
+		const checkedTop = checkVal(newTop, smallestEdge, dimensions._height);
 
-	const extracted = await sharp(`./images/${filename}`)
-		.extract({
-			left: checkedLeft,
-			top: checkedTop,
-			width: smallestEdge,
-			height: smallestEdge,
-		})
-		.toFile(`./out/${filename.split('.')[0]}.png`, (err, info) => {
-			if (err) throw err;
-			console.log('Success', info);
-		});
+		const extracted = await sharp(`./images/${filename}`)
+			.extract({
+				left: checkedLeft,
+				top: checkedTop,
+				width: smallestEdge,
+				height: smallestEdge,
+			})
+			.toFile(`./out/${filename.split('.')[0]}.png`, (err, info) => {
+				if (err) console.error(err);
+				console.log('Success', info);
+			});
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 export const createAvatars = async () => {
